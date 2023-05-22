ac164dcc4675587744c3ad593129a4372039ddf4
DragomirVladimir <drag.v777@gmail.com>
2023-05-22 18:28:23 +0300

    adeptive, cases

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
 $(function () {
 
-
   $('#fullpage').fullpage({
     credits: { enabled: false },
     licenseKey: 'gplv3-license',
     menu: '#navigation',
+    verticalCentered: false,
+    responsiveWidth: 1025,
+    responsiveHeight: 769,
 
     onLeave: function (origin, destination, direction) {
 
       if (destination.index !== 0) {
-        $('.header').addClass('header--hidden')
+        $('.header').addClass('header--hidden');
 
       } else {
-        $('.header').removeClass('header--hidden')
+        $('.header').removeClass('header--hidden');
       }
     },
 
   });
+
+  if ($(window).width() <= 1025 || $(window).height() <= 769) {
+    $('.section, .fp-overflow').css({
+      'min-height': 'auto',
+      'max-height': 'fit-content',
+    });
+  };
+
 
 
   $('.services__list').slick({
@@ -24,6 +34,14 @@ $(function () {
     arrows: true,
     infinite: false,
     variableWidth: true,
+
+    responsive: [{
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+      }
+    }]
 
   });
 
@@ -43,4 +61,44 @@ $(function () {
     observer.observe(this);
   });
 
+
+
+
+  $('.burger-btn').on('click', function () {
+
+    $('.burger-btn').toggleClass('burger-btn--active');
+
+    $('.nav__menu').toggleClass('nav__menu--active');
+
+    $('body').toggleClass('disable-scroll');
+
+  })
+
+  $('.nav__menu-link, .logo').on('click', function () {
+
+    $('.nav__menu').removeClass('nav__menu--active');
+
+    $('body').removeClass('disable-scroll');
+
+    $('.burger-btn').removeClass('burger-btn--active');
+
+  });
+
+
+
+
+  $('.cases__item').on('mouseover', function () {
+
+    $('.cases__item').removeClass('cases__item--active');
+    $(this).addClass('cases__item--active');
+
+  })
+
+  $('.cases__item').on('mouseleave', function () {
+    $(this).removeClass('cases__item--active');
+    $('.cases__item').first().addClass('cases__item--active');
+  });
+
+
 })
+
