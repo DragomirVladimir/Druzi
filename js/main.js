7714376da9d30b1e66e502aef55928a006f70e65
DragomirVladimir <drag.v777@gmail.com>
2023-05-26 17:41:42 +0300

    added form

--- js/main.js
+++ js/main.js
@@ -120,112 +120,33 @@ $(function () {
   })
 
   $('.cases__item').on('mouseleave', function () {
+
     $(this).removeClass('cases__item--active');
+
     $('.cases__item').first().addClass('cases__item--active');
+
   });
 
+  $('.services__card-btn').on('click', function () {
+
+    $('.services__card').removeClass('services__card--active');
+
+    $(this).closest('.services__card').addClass('services__card--active');
+
+  })
+
+  $('.main-screen__dscr-btn').on('click', function () {
+
+    $('.modal').addClass('modal--active');
+
+    $(document).keyup(function (e) {
+      if (e.key === "Escape" || e.keyCode === 27) {
+        $('.modal').removeClass('modal--active');
+        console.log('esc')
+      }
+    });
+
+  })
 
 })
-
-// (function () {
-//   // Кэширование повторно используемых элементов
-//   const $header = $('.header');
-//   const $elementHide = $('.element-hide');
-//   const $burgerBtn = $('.burger-btn');
-//   const $navMenu = $('.nav__menu');
-//   const $casesItems = $('.cases__item');
-
-//   // Функция для обработки события onLeave
-//   function handleOnLeave(origin, destination, direction) {
-//     if (destination.index !== 0) {
-//       $header.addClass('header--hidden');
-//     } else {
-//       $header.removeClass('header--hidden');
-//     }
-//   }
-
-//   // Функция для обработки события onEntry
-//   function handleOnEntry(entry, observer) {
-//     entry.forEach(change => {
-//       if (change.isIntersecting) {
-//         $(change.target).addClass('element-show');
-//       }
-//     });
-//   }
-
-//   // Опции для IntersectionObserver
-//   const intersectionOptions = {
-//     threshold: [0.3]
-//   };
-
-//   // Создание IntersectionObserver и наблюдение за элементами
-//   const observer = new IntersectionObserver(handleOnEntry, intersectionOptions);
-//   $elementHide.each(function () {
-//     observer.observe(this);
-//   });
-
-//   // Обработчик события click для кнопки бургера
-//   function toggleBurgerMenu() {
-//     $burgerBtn.toggleClass('burger-btn--active');
-//     $navMenu.toggleClass('nav__menu--active');
-//     $('body').toggleClass('disable-scroll');
-//   }
-
-//   // Обработчик события click для ссылок меню и логотипа
-//   function resetBurgerMenu() {
-//     $navMenu.removeClass('nav__menu--active');
-//     $('body').removeClass('disable-scroll');
-//     $burgerBtn.removeClass('burger-btn--active');
-//   }
-
-//   // Назначение обработчиков событий
-//   $burgerBtn.on('click', toggleBurgerMenu);
-//   $('.nav__menu-link, .logo').on('click', resetBurgerMenu);
-
-//   // Обработка событий mouseover и mouseleave для элементов cases__item
-//   $casesItems.hover(
-//     function () {
-//       $casesItems.removeClass('cases__item--active');
-//       $(this).addClass('cases__item--active');
-//     },
-//     function () {
-//       $(this).removeClass('cases__item--active');
-//       $casesItems.first().addClass('cases__item--active');
-//     }
-//   );
-
-//   // Инициализация fullpage.js
-//   $('#fullpage').fullpage({
-//     credits: { enabled: false },
-//     licenseKey: 'gplv3-license',
-//     menu: '#navigation',
-//     verticalCentered: false,
-//     responsiveWidth: 1025,
-//     responsiveHeight: 769,
-//     onLeave: handleOnLeave,
-//   });
-
-//   // Установка CSS-свойств для section и fp-overflow на мобильных устройствах
-//   if ($(window).width() <= 1025 || $(window).height() <= 769) {
-//     $('.section, .fp-overflow').css({
-//       'min-height': 'auto',
-//       'max-height': 'fit-content'
-//     });
-//   }
-
-//   // Инициализация слайдера slick.js
-//   $('.services__list').slick({
-//     arrows: true,
-//     infinite: false,
-//     variableWidth: true,
-//     responsive: [{
-//       breakpoint: 576,
-//       settings: {
-//         slidesToShow: 1,
-//         centerMode: true
-//       }
-//     }]
-//   });
-
-// })();
 
